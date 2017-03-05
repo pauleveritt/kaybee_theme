aaf72a00bc7bc5b8433736633c37f40477ded893
Paul Everitt <pauleveritt@me.com>
2017-03-05 09:45:49 -0500

    SCSS is working, albeit without source maps it appears.

--- webpack.config.js
+++ webpack.config.js
@@ -16,25 +16,53 @@ const config = {
     filename: '[name].bundle.js',
   },
   module: {
-    rules: [{
-      test: /\.(png|jpg)$/,
-      include: path.resolve(__dirname, 'src'),
-      use: [{
-        loader: 'url-loader',
-        options: {limit: 10000} // Convert images < 10k to base64 strings
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          {
+            loader: 'url-loader',
+            options: {limit: 10000} // Convert images < 10k to base64 strings
+          }]
+      },
+
+      {
+        test: /\.css$/,
+        exclude: '/node_modules/',
+        use: ExtractTextPlugin.extract({
+          fallbackLoader: [{
+            loader: 'style-loader',
+          }],
+          use: [{
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
+            },
+          }, {
+            loader: 'postcss-loader',
+          }],
+        }),
+      },
+
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+        // loader: extractCSS.extract(['css-loader', 'sass-loader'])
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: [{
+          loader: 'babel-loader',
+          options: {presets: ['es2015']}
+        }]
       }]
-    }, {
-      test: /\.scss$/,
-      include: path.resolve(__dirname, 'src'),
-      loader: extractCSS.extract(['css-loader', 'sass-loader'])
-    }, {
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'src'),
-      use: [{
-        loader: 'babel-loader',
-        options: {presets: ['es2015']}
-      }]
-    }]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
@@ -43,8 +71,7 @@ const config = {
         // this assumes your vendor imports exist in the node_modules directory
         return module.context && module.context.indexOf('node_modules') !== -1
       }
-    }),
-    extractCSS
+    })
   ]
 }
 
